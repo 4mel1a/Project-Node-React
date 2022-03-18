6e29b5a0e7f43ba1d2b82559a5225dff4088fdaa
Amelia Siti Nur Azizah <63657315+4mel1a@users.noreply.github.com>
2022-03-18 12:18:32 +0700

    Add files via upload

---
+++ backend/document/pdf.js
@@ -0,0 +1,132 @@
+module.exports = ({ nama_member, tgl, tgl_bayar, jenis_paket, harga, qty }) => {
+    const today = new Date();
+return `
+    <!doctype html>
+    <html>
+       <head>
+          <meta charset="utf-8">
+          <title>PDF Result Template</title>
+          <style>
+             .invoice-box {
+             max-width: 800px;
+             margin: auto;
+             padding: 30px;
+             border: 1px solid #eee;
+             box-shadow: 0 0 10px rgba(0, 0, 0, .15);
+             font-size: 16px;
+             line-height: 24px;
+             font-family: 'Helvetica Neue', 'Helvetica',
+             color: #555;
+             }
+             .margin-top {
+             margin-top: 50px;
+             }
+             .justify-center {
+             text-align: center;
+             }
+             .invoice-box table {
+             width: 100%;
+             line-height: inherit;
+             text-align: left;
+             }
+             .invoice-box table td {
+             padding: 5px;
+             vertical-align: top;
+             }
+             .invoice-box table tr td:nth-child(2) {
+             text-align: right;
+             }
+             .invoice-box table tr.top table td {
+             padding-bottom: 20px;
+             }
+             .invoice-box table tr.top table td.title {
+             font-size: 45px;
+             line-height: 45px;
+             color: #333;
+             }
+             .invoice-box table tr.information table td {
+             padding-bottom: 40px;
+             }
+             .invoice-box table tr.heading td {
+             background: #eee;
+             border-bottom: 1px solid #ddd;
+             font-weight: bold;
+             }
+             .invoice-box table tr.details td {
+             padding-bottom: 20px;
+             }
+             .invoice-box table tr.item td {
+             border-bottom: 1px solid #eee;
+             }
+             .invoice-box table tr.item.last td {
+             border-bottom: none;
+             }
+             .invoice-box table tr.total td:nth-child(2) {
+             border-top: 2px solid #eee;
+             font-weight: bold;
+             }
+             @media only screen and (max-width: 600px) {
+             .invoice-box table tr.top table td {
+             width: 100%;
+             display: block;
+             text-align: center;
+             }
+             .invoice-box table tr.information table td {
+             width: 100%;
+             display: block;
+             text-align: center;
+             }
+             }
+          </style>
+       </head>
+       <body>
+          <div class="invoice-box">
+             <table cellpadding="0" cellspacing="0">
+                <tr class="top">
+                   <td colspan="2">
+                      <table>
+                         <tr>
+                            <td class="title"><img  src="https://i2.wp.com/cleverlogos.co/wp-content/uploads/2018/05/reciepthound_1.jpg?fit=800%2C600&ssl=1"
+                               style="width:100%; max-width:156px;"></td>
+                            <td>
+                               Datum: ${`${today.getDate()}. ${today.getMonth() + 1}. ${today.getFullYear()}.`}
+                            </td>
+                         </tr>
+                      </table>
+                   </td>
+                </tr>
+                <tr class="information">
+                   <td rowspan="3">
+                      <table>
+                         <tr>
+                            <td>
+                               Member Name: ${nama_member}
+                            </td>
+                            <td>
+                               Tanggal Masuk: ${tgl}
+                            </td>
+                            <td>
+                               Tanggal Bayar: ${tgl_bayar}
+                            </td>
+                         </tr>
+                      </table>
+                   </td>
+                </tr>
+                <tr class="heading">
+                   <td>Jenis Paket</td>
+                   <td>Harga</td>
+                   <td>Qty</td>
+                </tr>
+                <tr class="item">
+                   <td>${jenis_paket}$</td>
+                   <td>${harga}$</td>
+                   <td>${qty}$</td>
+                </tr>
+             </table>
+             <br />
+             <h1 class="justify-center">Total Harga: ${parseInt(harga) * parseInt(qty)}$</h1>
+          </div>
+       </body>
+    </html>
+    `;
+};
